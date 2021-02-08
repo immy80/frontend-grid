f08a362a6f2b8bf572cff8a55eb31704ce8210f7
= <immy01@hotmail.com>
2021-02-08 21:13:28 +0000

    swiper slider

--- main.js
+++ main.js
@@ -13,3 +13,31 @@ new Masonry('#post .grid',{
     itemSelector: '.grid-item',
     gutter: 20
 })
+
+//swiper library
+
+new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView:5,
+    autoplay:{
+        delay:3000
+    },
+// responsive break points using ratio
+breakpoints:{
+    '@0':{
+        slidesPerView: 2
+    },
+    '@1.00':{
+        slidesPerView :3
+    },
+    '@1.25':{
+        slidesPerView :4
+    },
+    '@1.50':{
+        slidesPerView :5
+    },
+    
+
+}
+})
